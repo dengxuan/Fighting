36ae7102551cf2f789638eaca529b263ebfe6259
zhangpan <zhangpan@ZHANGPAN-PC>
2018-03-07 15:21:47 +0800

    添加穗彩

---
+++ src/Baibaocp.LotteryDispatching.Suicai.Abstractions/DispatcherOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baibaocp.LotteryDispatcher.Suicai
+{
+    public class DispatcherOptions
+    {
+        public string MerchanterId { get; set; }
+
+        public string Url { get; set; }
+
+        public string SecretKey { get; set; }
+    }
+}
