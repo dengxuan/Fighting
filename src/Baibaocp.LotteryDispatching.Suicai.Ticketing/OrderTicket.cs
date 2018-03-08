eb5a0b0af68ca62797ea18848d20b2821ec3833a
zhangpan <zhangpan@ZHANGPAN-PC>
2018-03-08 18:12:05 +0800

    添加穗彩出票查询，中奖查询

---
+++ src/Baibaocp.LotteryDispatching.Suicai.Ticketing/OrderTicket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baibaocp.LotteryDispatching.Suicai.Ticketing
+{
+    public class OrderTicket
+    {
+        public List<Ticket> orderList { get; set; }
+    }
+    public class Ticket
+    {
+        public string orderId { get; set; }
+
+    }
+}
