d04d2df8acec1d2c30abfa9ee560ac90f5d2b2aa
zhangpan <zhangpan@ZHANGPAN-PC>
2018-03-16 17:54:14 +0800

    添加领航渠道

---
+++ src/Baibaocp.LotteryDispatching.Linghang.Abstractions/DependencyInjection/LinghangExecuteDispataherExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Baibaocp.LotteryDispatching;
+using Baibaocp.LotteryDispatching.Abstractions;
+using Baibaocp.LotteryDispatching.DependencyInjection.Builder;
+using Baibaocp.LotteryDispatching.Linghang.Dispatchers;
+using Baibaocp.LotteryDispatching.Linghang.Handlers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Baibaocp.LotteryDispatching.Linghang.Abstractions.DependencyInjection
+{
+    public static class LinghangExecuteDispataherExtensions
+    {
+        public static LotteryDispatcherBuilder UseJinghangExecuteDispatcher(this LotteryDispatcherBuilder lotteryDispatcherBuilder, DispatcherConfiguration dispatcherConfiguration)
+        {
+            lotteryDispatcherBuilder.Services.AddSingleton<IOrderingDispatcher, OrderingExecuteDispatcher>();
+            lotteryDispatcherBuilder.Services.AddSingleton<IQueryingDispatcher, QueryingExecuteDispatcher>();
+            lotteryDispatcherBuilder.Services.AddSingleton(dispatcherConfiguration);
+            return lotteryDispatcherBuilder;
+        }
+    }
+}
---
+++ src/Baibaocp.LotteryDispatching.Linghang.Abstractions/Dispatchers/TicketingExecuteDispatcher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baibaocp.LotteryDispatching.Linghang.Abstractions.Dispatchers
+{
+    class TicketingExecuteDispatcher
+    {
+    }
+}
