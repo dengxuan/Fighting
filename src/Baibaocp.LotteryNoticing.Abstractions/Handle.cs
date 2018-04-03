c2eb504760e03be0b9493267beaa5f3eeb0590fb
dengxuan <dengxuan@mail.com>
2018-04-03 12:04:24 +0800

    修改通知日志

--- src/Baibaocp.LotteryNoticing.Abstractions/Handle.cs
+++ src/Baibaocp.LotteryNoticing.Abstractions/Handle.cs
@@ -11,5 +11,10 @@ namespace Baibaocp.LotteryNotifier.Abstractions
         public string Message { get; set; }
 
         public string Data { get; set; }
+
+        public override string ToString()
+        {
+            return $"Handlle [ Code: {Code} Message: {Message} Data: {Data} ]";
+        }
     }
 }
