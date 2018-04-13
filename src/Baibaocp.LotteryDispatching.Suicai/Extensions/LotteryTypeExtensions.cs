eee59fcb5317d164576ab05efe649eab896389e7
zhangpan <zhangpan@ZHANGPAN-PC>
2018-04-13 16:12:50 +0800

    修改穗彩快三投注格式

--- src/Baibaocp.LotteryDispatching.Suicai/Extensions/LotteryTypeExtensions.cs
+++ src/Baibaocp.LotteryDispatching.Suicai/Extensions/LotteryTypeExtensions.cs
@@ -17,91 +17,191 @@ namespace Baibaocp.LotteryDispatching.Suicai.Abstractions.Extensions
                 case (int)LotteryTypes.ZcSfc:
                 case (int)LotteryTypes.ZcR9: return "2006";
                 case (int)LotteryTypes.JxKs: return "19";
+                case (int)LotteryTypes.GxKs: return "9";
                 case (int)LotteryTypes.CqSsc: return "404";
                 case (int)LotteryTypes.SdSyxw: return "2005";
                 case (int)LotteryTypes.GxSyxw: return "2007";
+                case (int)LotteryTypes.HbSyxw: return "2008";
                 default: throw new ArgumentException("LotteryType Not Support: {0}", lotteryType.ToString());
             }
         }
 
-        public static string ToPlay(this int playType, bool type)
+        public static string ToPlay(this int playType,int lottery, bool type)
         {
-            switch (playType)
+            string play = string.Empty;
+            switch (lottery)
             {
-                case (int)PlayTypes.Ssq_Single: return "3000";
-                case (int)PlayTypes.Ssq_Multiple: return "3001";
-                case (int)PlayTypes.Ssq_FixedUnset: return "3002";
-                case (int)PlayTypes.Dlt_Single:
-                    if (type == true)
+                case (int)LotteryTypes.Dlt:
+                    switch (playType)
                     {
-                        return "11";
+                        case (int)PlayTypes.Dlt_Single:
+                            if (type == true)
+                            {
+                                play = "11";
+                            }
+                            else
+                            {
+                                play = "1";
+                            }
+                            break;
+                        case (int)PlayTypes.Dlt_Multiple: play = "4"; break;
+                        case (int)PlayTypes.Dlt_FixedUnset: play = "7"; break;
                     }
-                    else
+                    break;
+                case (int)LotteryTypes.Ssq:
+                    switch (playType)
                     {
-                        return "1";
+                        case (int)PlayTypes.Ssq_Single: play = "3000"; break;
+                        case (int)PlayTypes.Ssq_Multiple: play = "3001"; break;
+                        case (int)PlayTypes.Ssq_FixedUnset: play = "3002"; break;
                     }
-                case (int)PlayTypes.Dlt_Multiple:return "4";
-                case (int)PlayTypes.Dlt_FixedUnset:return "7";
-                case (int)PlayTypes.Pls_FrontSingle:return "1";
-                case (int)PlayTypes.Pls_FrontMultiple:return "2";
-                case (int)PlayTypes.Pls_AnyThreeSingle:return "8";
-                case (int)PlayTypes.Pls_AnyThreeMultiple:return "6";
-                case (int)PlayTypes.Pls_AnySixSingle:return "4";
-                case (int)PlayTypes.Pls_AnySixMultiple: return "5";
-                case (int)PlayTypes.Plw_FrontSingle:return "1";
-                case (int)PlayTypes.Plw_FrontMultiple:return "2";
-                case (int)PlayTypes.Ks_ThreeDiffSingle: 
-                case (int)PlayTypes.Ks_TowSameSingle:
-                case (int)PlayTypes.Ks_ThreeSameSingle: return "9300";
-                case (int)PlayTypes.Ks_SumValue: return "9330";
-                case (int)PlayTypes.Ks_TowSameAll: return "9320";
-                case (int)PlayTypes.Ks_ThreeSameAll: return "9310";
-                case (int)PlayTypes.Ks_TowDiffSingle: return "9340";
-                case (int)PlayTypes.Ks_ThreeSeriesAll: return "9350";
-                case (int)PlayTypes.Sfc_Single:
-                case (int)PlayTypes.Sfc_Multiple: return "6001";
-                case (int)PlayTypes.Rj_Single:
-                case (int)PlayTypes.Rj_Multiple: return "6002";
-                case (int)PlayTypes.Syxw_AnyTowSingle:
-                case (int)PlayTypes.Syxw_AnyTownMultiple: return "24001";
-                case (int)PlayTypes.Syxw_AnyTowFixedUnset: return "24002";
-                case (int)PlayTypes.Syxw_AnyThreeSingle:
-                case (int)PlayTypes.Syxw_AnyThreeMultiple: return "24003";
-                case (int)PlayTypes.Syxw_AnyThreeFixedUnset: return "24004";
-                case (int)PlayTypes.Syxw_AnyFourSingle:
-                case (int)PlayTypes.Syxw_AnyFourMultiple: return "24005";
-                case (int)PlayTypes.Syxw_AnyFourFixedUnset: return "24006";
-                case (int)PlayTypes.Syxw_AnyFiveSingle:
-                case (int)PlayTypes.Syxw_AnyFiveMultiple: return "24007";
-                case (int)PlayTypes.Syxw_AnyFiveFixedUnset: return "24008";
-                case (int)PlayTypes.Syxw_AnySixSingle:
-                case (int)PlayTypes.Syxw_AnySixMultiple: return "24009";
-                case (int)PlayTypes.Syxw_AnySixFixedUnset: return "24010";
-                case (int)PlayTypes.Syxw_AnySevenSingle:
-                case (int)PlayTypes.Syxw_AnySevenMultiple: return "24011";
-                case (int)PlayTypes.Syxw_AnySevenFixedUnset: return "24012";
-                case (int)PlayTypes.Syxw_AnyEightSingle:
-                case (int)PlayTypes.Syxw_AnyEightMultiple: return "24013";
-                case (int)PlayTypes.Syxw_FrontOneSingle:
-                case (int)PlayTypes.Syxw_FrontOneMultiple: return "24014";
-                case (int)PlayTypes.Syxw_FrontTowFixedPositionSingle:
-                case (int)PlayTypes.Syxw_FrontTowFixedPositionMultiple: return "24015";
-                case (int)PlayTypes.Syxw_FrontTowAnyPositionSingle:
-                case (int)PlayTypes.Syxw_FrontTowAnyPositionMultiple: return "24016";
-                case (int)PlayTypes.Syxw_FrontTowAnyPositionFixedUnset: return "24017";
-                case (int)PlayTypes.Syxw_FrontThreeFixedPositionSingle:
-                case (int)PlayTypes.Syxw_FrontThreeFixedPositionMultiple: return "24018";
-                case (int)PlayTypes.Syxw_FrontThreeAnyPositionSingle:
-                case (int)PlayTypes.Syxw_FrontThreeAnyPositionMultiple: return "24019";
-                case (int)PlayTypes.Syxw_FrontThreeAnyPositionFixedUnset: return "24020";
-                case (int)PlayTypes.Sd_FrontSingle: return "2000";
-                case (int)PlayTypes.Sd_FrontMultiple: return "2020";
-                case (int)PlayTypes.Sd_AnyThreeSingle: return "2010";
-                case (int)PlayTypes.Sd_AnySixSingle: return "2011";
-                case (int)PlayTypes.Sd_AnyThreeMultiple: return "2030";
-                case (int)PlayTypes.Sd_AnySixMultiple: return "2031";
-                default: throw new ArgumentException("PlayType Not Support: {0}", playType.ToString());
+                    break;
+                case (int)LotteryTypes.Pls:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Pls_FrontSingle: play = "1"; break;
+                        case (int)PlayTypes.Pls_FrontMultiple: play = "2"; break;
+                        case (int)PlayTypes.Pls_AnyThreeSingle: play = "8"; break;
+                        case (int)PlayTypes.Pls_AnyThreeMultiple: play = "6"; break;
+                        case (int)PlayTypes.Pls_AnySixSingle: play = "4"; break;
+                        case (int)PlayTypes.Pls_AnySixMultiple: play = "5"; break;
+                    }
+                    break;
+                case (int)LotteryTypes.Plw:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Plw_FrontSingle: play = "1"; break;
+                        case (int)PlayTypes.Plw_FrontMultiple: play = "2"; break;
+                    }
+                    break;
+                case (int)LotteryTypes.GxKs:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Ks_ThreeDiffSingle: play = "9330"; break;
+                        case (int)PlayTypes.Ks_TowSameSingle: play = "9321"; break;
+                        case (int)PlayTypes.Ks_ThreeSameSingle: play = "9311"; break;
+                        case (int)PlayTypes.Ks_SumValue: play = "9300"; break;
+                        case (int)PlayTypes.Ks_TowSameAll: play = "9320"; break;
+                        case (int)PlayTypes.Ks_ThreeSameAll: play = "9310"; break;
+                        case (int)PlayTypes.Ks_TowDiffSingle: play = "9340"; break;
+                        case (int)PlayTypes.Ks_ThreeSeriesAll: play = "9350"; break;
+                    }
+                    break;
+                case (int)LotteryTypes.JxKs:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Ks_ThreeDiffSingle:
+                        case (int)PlayTypes.Ks_TowSameSingle:
+                        case (int)PlayTypes.Ks_ThreeSameSingle: play = "9300"; break;
+                        case (int)PlayTypes.Ks_SumValue: play = "9330"; break;
+                        case (int)PlayTypes.Ks_TowSameAll: play = "9320"; break;
+                        case (int)PlayTypes.Ks_ThreeSameAll: play = "9310"; break;
+                        case (int)PlayTypes.Ks_TowDiffSingle: play = "9340"; break;
+                        case (int)PlayTypes.Ks_ThreeSeriesAll: play = "9350"; break;
+                    }
+                    break;
+                case (int)LotteryTypes.ZcSfc:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Sfc_Single:
+                        case (int)PlayTypes.Sfc_Multiple: play = "6001"; break;
+                    }
+                    break;
+                case (int)LotteryTypes.ZcR9:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Rj_Single:
+                        case (int)PlayTypes.Rj_Multiple: play = "6002"; break;
+                    }
+                    break;
+                case (int)LotteryTypes.GxSyxw:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Syxw_AnyTowSingle:
+                        case (int)PlayTypes.Syxw_AnyTownMultiple: play = "24001"; break;
+                        case (int)PlayTypes.Syxw_AnyTowFixedUnset: play = "24002"; break;
+                        case (int)PlayTypes.Syxw_AnyThreeSingle:
+                        case (int)PlayTypes.Syxw_AnyThreeMultiple: play = "24003"; break;
+                        case (int)PlayTypes.Syxw_AnyThreeFixedUnset: play = "24004"; break;
+                        case (int)PlayTypes.Syxw_AnyFourSingle:
+                        case (int)PlayTypes.Syxw_AnyFourMultiple: play = "24005"; break;
+                        case (int)PlayTypes.Syxw_AnyFourFixedUnset: play = "24006"; break;
+                        case (int)PlayTypes.Syxw_AnyFiveSingle:
+                        case (int)PlayTypes.Syxw_AnyFiveMultiple: play = "24007"; break;
+                        case (int)PlayTypes.Syxw_AnyFiveFixedUnset: play = "24008"; break;
+                        case (int)PlayTypes.Syxw_AnySixSingle:
+                        case (int)PlayTypes.Syxw_AnySixMultiple: play = "24009"; break;
+                        case (int)PlayTypes.Syxw_AnySixFixedUnset: play = "24010"; break;
+                        case (int)PlayTypes.Syxw_AnySevenSingle:
+                        case (int)PlayTypes.Syxw_AnySevenMultiple: play = "24011"; break;
+                        case (int)PlayTypes.Syxw_AnySevenFixedUnset: play = "24012"; break;
+                        case (int)PlayTypes.Syxw_AnyEightSingle:
+                        case (int)PlayTypes.Syxw_AnyEightMultiple: play = "24013"; break;
+                        case (int)PlayTypes.Syxw_FrontOneSingle:
+                        case (int)PlayTypes.Syxw_FrontOneMultiple: play = "24014"; break;
+                        case (int)PlayTypes.Syxw_FrontTowFixedPositionSingle:
+                        case (int)PlayTypes.Syxw_FrontTowFixedPositionMultiple: play = "24015"; break;
+                        case (int)PlayTypes.Syxw_FrontTowAnyPositionSingle:
+                        case (int)PlayTypes.Syxw_FrontTowAnyPositionMultiple: play = "24016"; break;
+                        case (int)PlayTypes.Syxw_FrontTowAnyPositionFixedUnset: play = "24017"; break;
+                        case (int)PlayTypes.Syxw_FrontThreeFixedPositionSingle:
+                        case (int)PlayTypes.Syxw_FrontThreeFixedPositionMultiple: play = "24018"; break;
+                        case (int)PlayTypes.Syxw_FrontThreeAnyPositionSingle:
+                        case (int)PlayTypes.Syxw_FrontThreeAnyPositionMultiple: play = "24019"; break;
+                        case (int)PlayTypes.Syxw_FrontThreeAnyPositionFixedUnset: play = "24020"; break;
+                    }
+                    break;
+                case (int)LotteryTypes.HbSyxw:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Syxw_AnyTowSingle:
+                        case (int)PlayTypes.Syxw_AnyTownMultiple: play = "23001"; break;
+                        case (int)PlayTypes.Syxw_AnyTowFixedUnset: play = "23002"; break;
+                        case (int)PlayTypes.Syxw_AnyThreeSingle:
+                        case (int)PlayTypes.Syxw_AnyThreeMultiple: play = "23003"; break;
+                        case (int)PlayTypes.Syxw_AnyThreeFixedUnset: play = "23004"; break;
+                        case (int)PlayTypes.Syxw_AnyFourSingle:
+                        case (int)PlayTypes.Syxw_AnyFourMultiple: play = "23005"; break;
+                        case (int)PlayTypes.Syxw_AnyFourFixedUnset: play = "23006"; break;
+                        case (int)PlayTypes.Syxw_AnyFiveSingle:
+                        case (int)PlayTypes.Syxw_AnyFiveMultiple: play = "23007"; break;
+                        case (int)PlayTypes.Syxw_AnyFiveFixedUnset: play = "23008"; break;
+                        case (int)PlayTypes.Syxw_AnySixSingle:
+                        case (int)PlayTypes.Syxw_AnySixMultiple: play = "23009"; break;
+                        case (int)PlayTypes.Syxw_AnySixFixedUnset: play = "23010"; break;
+                        case (int)PlayTypes.Syxw_AnySevenSingle:
+                        case (int)PlayTypes.Syxw_AnySevenMultiple: play = "23011"; break;
+                        case (int)PlayTypes.Syxw_AnySevenFixedUnset: play = "23012"; break;
+                        case (int)PlayTypes.Syxw_AnyEightSingle:
+                        case (int)PlayTypes.Syxw_AnyEightMultiple: play = "23013"; break;
+                        case (int)PlayTypes.Syxw_FrontOneSingle:
+                        case (int)PlayTypes.Syxw_FrontOneMultiple: play = "23014"; break;
+                        case (int)PlayTypes.Syxw_FrontTowFixedPositionSingle:
+                        case (int)PlayTypes.Syxw_FrontTowFixedPositionMultiple: play = "23015"; break;
+                        case (int)PlayTypes.Syxw_FrontTowAnyPositionSingle:
+                        case (int)PlayTypes.Syxw_FrontTowAnyPositionMultiple: play = "23016"; break;
+                        case (int)PlayTypes.Syxw_FrontTowAnyPositionFixedUnset: play = "23017"; break;
+                        case (int)PlayTypes.Syxw_FrontThreeFixedPositionSingle:
+                        case (int)PlayTypes.Syxw_FrontThreeFixedPositionMultiple: play = "23018"; break;
+                        case (int)PlayTypes.Syxw_FrontThreeAnyPositionSingle:
+                        case (int)PlayTypes.Syxw_FrontThreeAnyPositionMultiple: play = "23019"; break;
+                        case (int)PlayTypes.Syxw_FrontThreeAnyPositionFixedUnset: play = "23020"; break;
+                    }
+                    break;
+                case (int)LotteryTypes.Sd:
+                    switch (playType)
+                    {
+                        case (int)PlayTypes.Sd_FrontSingle: play = "2000"; break;
+                        case (int)PlayTypes.Sd_FrontMultiple: play = "2020"; break;
+                        case (int)PlayTypes.Sd_AnyThreeSingle: play = "2010"; break;
+                        case (int)PlayTypes.Sd_AnySixSingle: play = "2011"; break;
+                        case (int)PlayTypes.Sd_AnyThreeMultiple: play = "2030"; break;
+                        case (int)PlayTypes.Sd_AnySixMultiple: play = "2031"; break;
+                    }
+                    break;
+                default: throw new ArgumentException("玩法{0}不支持", playType.ToString());
             }
+            return play;
         }
 
         public static int ToIssueNumber(this string issueNumber, int lotteryType)
@@ -113,6 +213,7 @@ namespace Baibaocp.LotteryDispatching.Suicai.Abstractions.Extensions
                 case (int)LotteryTypes.CqSsc:
                 case (int)LotteryTypes.JxKs:
                 case (int)LotteryTypes.SdSyxw:
+                case (int)LotteryTypes.HbSyxw:
                 case (int)LotteryTypes.GxSyxw: return Convert.ToInt32(issueNumber);
                 case (int)LotteryTypes.Plw:
                 case (int)LotteryTypes.Pls:
@@ -133,7 +234,9 @@ namespace Baibaocp.LotteryDispatching.Suicai.Abstractions.Extensions
                 case (int)LotteryTypes.Sd:
                 case (int)LotteryTypes.CqSsc:
                 case (int)LotteryTypes.JxKs:
+                case (int)LotteryTypes.GxKs:
                 case (int)LotteryTypes.SdSyxw:
+                case (int)LotteryTypes.HbSyxw:
                 case (int)LotteryTypes.GxSyxw: return issueNumber.ToString();
                 case (int)LotteryTypes.Pls:
                 case (int)LotteryTypes.Plw:
